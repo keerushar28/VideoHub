e0ce5b5afaf113523c557ab5c7c9b6b881e9ee1e
Kiran Gautam <gautamkiran878@gmail.com>
2024-08-18 09:16:09 +0545

    Email Configuration Set Up

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,20 @@
+import transporter from "../config/emailConfig.js";
+
+const sendEmail = async(to,subject,text) => {
+    try {
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to,
+            subject,
+            text,
+        })
+        console.log("Email Send Successfully");
+        
+
+        
+    } catch (error) {
+        console.error("Error Sending Email", error)
+        
+    }
+}
+export default sendEmail;
