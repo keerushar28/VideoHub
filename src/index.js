e53c547915345ccf34d9f1dfcff5d26719b4cfd0
Kiran Gautam <gautamkiran878@gmail.com>
2024-08-22 20:31:46 +0545

    Custom API error and API response

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/dbconfig.js';
 import authRoutes from './routes/authRoutes.js';
+import { app } from './app.js'
+
 
 // Load environment variables from .env file
 dotenv.config({ path: './.env' });
@@ -10,11 +12,14 @@ dotenv.config({ path: './.env' });
 connectDB();
 
 // Initialize Express app
+/*
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+*/
+const port = process.env.PORT || 3000;
 
 // Routes
 app.use('/api/auth', authRoutes);
