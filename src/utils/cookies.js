504dc167311b058c851ff0d3dfeadb5758c7af2e
Kiran Gautam <gautamkiran878@gmail.com>
2024-08-26 19:57:21 +0545

    Cookie generated using jsonwebtoken

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,1 +1,15 @@
-import jwt from "json"
+import jwt from "jsonwebtoken"
+
+export const generateCookie = async (res,userID) => {
+    const token = jwt.sign({userID},process.env.SECRET_KEY,{expiresIn: "1d"})
+    res.cookie('jwt', token , {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 86400000 // 1 day in milliseconds
+        
+    })
+
+}
+
+
