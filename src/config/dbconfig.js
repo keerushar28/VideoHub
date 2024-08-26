78eb6720948662162627649dc619b1cd17bb2744
Kiran Gautam <gautamkiran878@gmail.com>
2024-08-26 19:56:36 +0545

    Datebase uri corrected

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-       const connectionInstance = await mongoose.connect(process.env.MONGO_URI);
+        const connectionInstance = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected Host: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error('MongoDB connection error:', error);
