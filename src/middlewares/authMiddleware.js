617342e2f7fb5ab4eeae9d6db9d11c1cf5d65ee0
Kiran Gautam <gautamkiran878@gmail.com>
2024-08-29 17:13:28 +0545

    Middleware Added

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken'
+
+export const verifyToken = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt
+        if (!token) {
+            res.status(400).json({ message: "No token Provided" })
+        }
+        const decoded = jwt.verify(token, process.env.SECRET_KEY)
+        if(!decoded)
+        {
+            res.status(400).json({message: "Unauthorized Access"})
+        }
+        req.userID = decoded.userID;
+        next();
+
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" })
+
+
+    }
+
+}
